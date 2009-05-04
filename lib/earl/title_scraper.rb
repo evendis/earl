7a5065e60618ed410d0aadf982011cfd637bc597
teejayvanslyke <tj@elctech.com>
2009-05-04 16:27:57 -0700

    Removed old scrapers.

--- lib/earl/title_scraper.rb
+++
@@ -1,7 +0,0 @@
-class Earl
-  class TitleScraper < Earl::Scraper
-    def scrape(response)
-      response.css('title').each {|t| return t.content}
-    end
-  end
-end
--- lib/earl/youtube_embed_code_scraper.rb
+++
@@ -1,7 +0,0 @@
-class Earl
-  class YoutubeEmbedCodeScraper < Earl::Scraper
-    def scrape(response)
-      response.css('#embed_code').each {|t| return t['value']}
-    end
-  end
-end
