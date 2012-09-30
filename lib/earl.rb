1bb0446ff4ed6de162e68c9c39290be398820b36
Paul Gallagher <gallagher.paul@gmail.com>
2012-09-30 13:04:16 +0800

    deprecate youtube scraper (should use embed now)

--- lib/earl.rb
+++ lib/earl.rb
@@ -7,5 +7,4 @@ end
 require 'earl/version'
 require 'earl/scraper'
 require 'earl/url'
-require 'earl/youtube_scraper'
 require 'earl/funny_or_die_scraper'
--- lib/earl/youtube_scraper.rb
+++
@@ -1,9 +0,0 @@
-module Earl
-  class YoutubeScraper < Earl::Scraper
-    match /^http\:\/\/www\.youtube\.com\/watch\?v\=.*$/
-
-    define_attribute :video do |doc|
-      doc.at('#embed_code')['value']
-    end
-  end
-end
